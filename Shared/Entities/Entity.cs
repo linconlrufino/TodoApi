d3663dd3e09e1c0374335e960d0a29e12c67019c
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-15 08:08:52 -0300

    abstract Entity class

---
+++ Shared/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Entities;
+
+internal abstract class Entity : IEquatable<Entity>
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; private set; }
+
+    public bool Equals(Entity? other)
+    {
+        return Id == other?.Id;
+    }
+}
