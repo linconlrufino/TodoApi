a06c8ae569119d6a2d593341126f0b6a9bd7c17d
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-17 19:39:54 -0300

    CreateTodoCommandTests

---
+++ Tests/CommandTests/CreateTodoCommandTests.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Tests.CommandTests;
+
+public class CreateTodoCommandTests
+{
+    [Fact]
+    [Trait("Category", "Command")]
+    public void Dado_um_comando_invalido()
+    {
+        //Arrange, Act, Assert
+        //Red, Greed, Refactor
+
+        Assert.False(false);
+    }
+
+    [Fact]
+    [Trait("Category", "Command")]
+    public void Dado_um_comando_valido()
+    {
+        //Arrange, Act, Assert
+        //Red, Greed, Refactor
+
+        Assert.False(false);
+    }
+}
