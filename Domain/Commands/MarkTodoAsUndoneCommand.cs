7ef59301151695b4806289c4c985b866eb7ad023
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-17 18:43:33 -0300

    MarkTodoAsUndoneCommand

---
+++ Domain/Commands/MarkTodoAsUndoneCommand.cs
@@ -0,0 +1,28 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Shared.Commands;
+
+namespace Domain.Commands;
+
+public class MarkTodoAsUndoneCommand : Notifiable<Notification>, ICommand
+{
+    public MarkTodoAsUndoneCommand() { }
+
+    public MarkTodoAsUndoneCommand(Guid id, string user)
+    {
+        Id = id;
+        User = user;
+    }
+
+    public Guid Id { get; set; }
+    public string User { get; set; }
+
+    public void Validate()
+    {
+        AddNotifications(
+            new Contract<Notification>()
+                .Requires()
+                .IsLowerThan(User, 6, "User", "Usuário inválido!")
+            );
+    }
+}
