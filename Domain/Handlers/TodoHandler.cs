933fad970853a85a082ba42ffc87c1521015b3ea
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-17 19:13:22 -0300

    TodoHandler

---
+++ Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Commands;
+using Domain.Repositories;
+using Flunt.Notifications;
+using Shared.Commands;
+using Shared.Handlers;
+
+namespace Domain.Handlers;
+
+public class TodoHandler :
+    Notifiable<Notification>,
+    IHandler<CreateTodoCommand>
+{
+    private readonly ITodoRepository _repository;
+
+    public TodoHandler(ITodoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public ICommandResult Handle(CreateTodoCommand command)
+    {
+
+    }
+}
