fc0b84583a9bc8b939437cdb41edea9b48aa5999
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-21 20:52:04 -0300

    TodoItem Mapping

--- Infra/Contexts/TodoContext.cs
+++ Infra/Contexts/TodoContext.cs
@@ -1,14 +1,17 @@
 ﻿using Domain.Entities;
+using Infra.Mappings;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Contexts;
 
 public class TodoContext : DbContext
 {
-    public TodoContext(DbContextOptions<TodoContext> options) : base(options)
-    {
-    }
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
 
     public DbSet<TodoItem> Todos { get; set; }
-    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TodoItemMap());
+    }
 }
---
+++ Infra/Mappings/TodoItemMap.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Mappings;
+
+public class TodoItemMap : IEntityTypeConfiguration<TodoItem>
+{
+    public void Configure(EntityTypeBuilder<TodoItem> builder)
+    {
+        builder.ToTable("Todo");
+        builder.Property(x => x.Id);
+        builder.Property(x => x.User).HasMaxLength(120).HasColumnType("varchar(120)");
+        builder.Property(x => x.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+        builder.Property(x => x.Done).HasColumnType("bit");
+        builder.Property(x => x.Date);
+        builder.HasIndex(x => x.User);
+    }
+}
