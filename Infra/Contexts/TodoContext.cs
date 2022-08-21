f0f66806a663ccb5ae0c72e6886348ab4e00a89c
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-21 20:25:16 -0300

    Entityframework

---
+++ Infra/Contexts/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Contexts;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> Todos { get; set; }
+    
+}
