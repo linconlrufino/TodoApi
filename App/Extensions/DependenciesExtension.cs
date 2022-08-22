5cee4561db0067a1a0894e0d6c033a8154749d59
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-21 22:24:55 -0300

    DependenciesExtensions and UseDataBaseInMemory

---
+++ App/Extensions/DependenciesExtension.cs
@@ -0,0 +1,21 @@
+﻿using Infra.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Extensions;
+
+public static class DependenciesExtension
+{
+    public static void AddSqlConnection(this IServiceCollection services, string connectString)
+    {
+        services.AddDbContext<TodoContext>(option => option.UseSqlServer(connectString));
+    }
+
+    public static void AddInMemoryDataBaseConnection(this IServiceCollection services, string connectString)
+    {
+        services.AddDbContext<TodoContext>(option => option.UseInMemoryDatabase(connectString));
+    }
+
+    public static void AddRepositories(this IServiceCollection services) { }
+
+    public static void AddServices(this IServiceCollection services) { }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,25 +1,31 @@
+using App.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//var conectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var conectionStringInMemory = builder.Configuration.GetConnectionString("DefaultConnectionInMemory");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddInMemoryDataBaseConnection(conectionStringInMemory);
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
 app.Run();
+
+
+
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
