22cb6845a19f4afeb4194752f0d4c06e4cd6234d
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-22 09:22:43 -0300

    finishing controller

--- App/Controllers/TodoCOntroller.cs
+++ App/Controllers/TodoCOntroller.cs
@@ -18,6 +18,54 @@ public class TodoController : ControllerBase
         return repository.GetAll("tolstoi");
     }
 
+    [Route("done")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllDone(
+        [FromServices] ITodoRepository repository)
+    {
+        return repository.GetAllDone("tolstoi");
+    }
+
+    [Route("undone")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllUndone(
+        [FromServices] ITodoRepository repository)
+    {
+        return repository.GetAllUndone("tolstoi");
+    }
+
+    [Route("done/today")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetDoneForToday(
+        [FromServices] ITodoRepository repository)
+    {
+        return repository.GetByPeriod("tolstoi", DateTime.Now.Date, true);
+    }
+
+    [Route("undone/today")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetInactiveForToday(
+        [FromServices] ITodoRepository repository)
+    {
+        return repository.GetByPeriod("tolstoi", DateTime.Now.Date, false);
+    }
+
+    [Route("done/tomorrow")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetDoneForTomorrow(
+    [FromServices] ITodoRepository repository)
+    {
+        return repository.GetByPeriod("tolstoi", DateTime.Now.Date.AddDays(1), true);
+    }
+
+    [Route("undone/tomorrow")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetUndoneForTomorrow(
+   [FromServices] ITodoRepository repository)
+    {
+        return repository.GetByPeriod("tolstoi", DateTime.Now.Date.AddDays(1), false);
+    }
+
     [Route("")]
     [HttpPost]
     public GenericCommandResult Create(
@@ -28,4 +76,30 @@ public class TodoController : ControllerBase
         return (GenericCommandResult)handler.Handle(command);
     }
 
+    [Route("")]
+    [HttpPut]
+    public GenericCommandResult Update(
+        [FromBody] UpdateTodoCommand command,
+        [FromServices] TodoHandler handler)
+    {
+        return (GenericCommandResult)handler.Handle(command);
+    }
+
+    [Route("mark-as-done")]
+    [HttpPut]
+    public GenericCommandResult MarkAsDone(
+        [FromBody] MarkTodoAsDoneCommand command,
+        [FromServices] TodoHandler handler)
+    {
+        return (GenericCommandResult)handler.Handle(command);
+    }
+
+    [Route("mark-as-undone")]
+    [HttpPut]
+    public GenericCommandResult MarkAsUndone(
+        [FromBody] MarkTodoAsUndoneCommand command,
+        [FromServices] TodoHandler handler)
+    {
+        return (GenericCommandResult)handler.Handle(command);
+    }
 }
